026c839fb26890d84471c422c0b165eaac4e3e14
adamr@catalyst-au.net <riddelladam42@gmail.com>
2015-07-18 22:51:11 +1000

    Add API tests for schedule event manipulation
        - GET /api/schedules
        - GET /api/schedule/<ID>
        - POST /api/schedule
        - PUT /api/schedule/<ID>
        - DELETE /api/schedule/<ID>

--- spec/api/schedule_spec.rb
+++ spec/api/schedule_spec.rb
@@ -7,30 +7,109 @@ describe Illuminati::API do
     Illuminati::API
   end
 
+  # Set up the data for test schedule events.
+  let(:schedule1_hash) {
+      {
+        :command => 'command',
+        :transition_time => 0,
+        :time => 1437209443,
+        :repeat => 1,
+        :cron_minute => '30',
+        :cron_hour => '17',
+        :cron_day => '*',
+        :cron_month => '*',
+        :cron_weekday => '1,6'
+      }
+  }
+  let(:schedule2_hash) {
+      {
+        :command => 'command_2',
+        :transition_time => 2
+      }
+  }
+
   context "with no schedule events" do
     it 'returns an empty collection of schedule events' do
       get '/api/schedules'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to eq("[]")
     end
+
+    it 'returns 404 when accessing nonexistent schedule event' do
+      get "/api/schedule/1"
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'creates a new schedule event' do
+      post_string = '/api/schedule?'
+      post_array = []
+      schedule1_hash.each do |key, value|
+        post_array += ["#{key}=#{value}"]
+      end
+      post_string += post_array.join("&")
+
+      expect {
+        post post_string
+        expect(last_response.status).to eq(201)
+      }.to change(Illuminati::Models::Schedule, :count).by(1)
+      schedule = Illuminati::Models::Schedule.last
+      schedule1_hash.each do |key, value|
+        expect(schedule[key]).to eq(value)
+      end
+    end
+
+    it 'fails to update a nonexistent schedule event' do
+      put '/api/schedule/1'
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'returns 200 when deleting nonexistent schedule event' do
+      delete '/api/schedule/1'
+      expect(last_response.status).to eq(200)
+    end
   end
-  context "with schedule events" do
-    schedule1 = {
-                   :command => "Make some crazy change",
-                   :transition_time => 2,
-                   :time => Time.now,
-                   :repeat => true,
-                   :cron_minute => '30',
-                   :cron_hour => '17',
-                   :cron_day => '*',
-                   :cron_month => '*',
-                   :cron_weekday => '1'
-                 }
-    Illuminati::Models::Schedule.create!(schedule1)
+
+  context "with schedule event" do
+    before do |each|
+      @schedule1 = Illuminati::Models::Schedule.create!(schedule1_hash)
+    end
+
     it "returns the collection of schedule events" do
       get '/api/schedules'
       expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq([schedule1].to_json)
+      expect(last_response.body).to eq([@schedule1].to_json)
+    end
+
+    it "returns a specific schedule event based on ID" do
+      get "/api/schedule/#{@schedule1.id}"
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq(@schedule1.to_json)
+    end
+
+    it "updates a schedule event based on ID" do
+      put_string = "/api/schedule/#{@schedule1.id}?"
+      put_array = []
+      schedule2_hash.each do |key, value|
+        put_array += ["#{key}=#{value}"]
+      end
+      put_string += put_array.join("&")
+
+      expect {
+        put put_string
+        expect(last_response.status).to eq(201)
+      }.to_not change(Illuminati::Models::Schedule, :count)
+      @schedule1.reload
+      schedule = Illuminati::Models::Schedule.find_by(_id: @schedule1.id)
+      schedule1_hash.merge(schedule2_hash).each do |key, value|
+        expect(schedule[key]).to eq(value)
+      end
+    end
+
+    it "removes a schedule event based on ID" do
+      expect {
+        delete "/api/schedule/#{@schedule1.id}"
+        expect(last_response.status).to eq(200)
+      }.to change(Illuminati::Models::Schedule, :count).by(-1)
     end
   end
 end
