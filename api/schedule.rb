d7a8df3980762abda4f5ad9b4cb42a336d449ef7
Adam Riddell <riddelladam42@gmail.com>
2015-07-19 00:20:27 +1000

    Implement GET and POST schedule event methods
        - GET schedule event by ID
        - POST new schedule event

--- api/schedule.rb
+++ api/schedule.rb
@@ -7,5 +7,36 @@ module Illuminati
         Illuminati::Models::Schedule.all.desc(:number).as_json
       end
     end
+
+    namespace :schedule do
+      desc "Returns schedule event by ID"
+      get ":_id" do
+        schedule = Illuminati::Models::Schedule.find(params[:_id])
+        error! "Not found", 404 unless schedule
+        schedule.as_json
+      end
+
+      desc "Creates a new schedule event"
+      post do
+        params do
+          requires :command, type: String
+          requires :time, type: Datetime, default: Time.now
+          optional :transition_time, type: Integer, values: 0..1800, default: 0
+          optional :repeat, type: Boolean, default: false
+          given :repeat do
+            cron_regexp = /^[0-9\/\*]+$/
+            optional :cron_minute, type: String, regexp: cron_regexp
+            optional :cron_hour, type: String, regexp: cron_regexp
+            optional :cron_day, type: String, regexp: cron_regexp
+            optional :cron_month, type: String, regexp: cron_regexp
+            optional :cron_weekday, type: String, regexp: cron_regexp
+            all_or_none_of :cron_minute, :cron_hour, :cron_day, :cron_month,
+              :cron_weekday
+          end
+        end
+        schedule = Illuminati::Models::Schedule.create!(declared(params))
+        schedule.as_json
+      end
+    end
   end
 end
