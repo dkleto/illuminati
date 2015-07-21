83531fe219d4e352e3267aa857ec3cec269c25eb
Adam Riddell <riddelladam42@gmail.com>
2015-07-21 22:10:24 +1000

    Implement schedule PUT and DELETE methods

--- api/schedule.rb
+++ api/schedule.rb
@@ -39,6 +39,44 @@ module Illuminati
         new_schedule = Illuminati::Models::Schedule.create!(schedule)
         new_schedule.as_json
       end
+
+      desc "Update a specific schedule event by ID"
+        params do
+          requires '_id'
+          optional :command, type: String
+          optional :time, type: DateTime, default: DateTime.now
+          optional :transition_time, type: Integer, values: 0..1800, default: 0
+          optional :repeat, type: Boolean, default: false
+          cron_regexp = /^[0-9\/\*,-]+$/
+          optional :cron_minute, type: String, regexp: cron_regexp
+          optional :cron_hour, type: String, regexp: cron_regexp
+          optional :cron_day, type: String, regexp: cron_regexp
+          optional :cron_month, type: String, regexp: cron_regexp
+          optional :cron_weekday, type: String, regexp: cron_regexp
+          all_or_none_of :cron_minute, :cron_hour, :cron_day, :cron_month,
+              :cron_weekday
+        end
+      put ':_id' do
+        schedule = Illuminati::Models::Schedule.find(params[:_id])
+        error! "Not Found", 404 unless schedule
+        values = Hash.new
+        declared(params).each do |key, value|
+          values[key] = value
+        end
+        schedule.update_attributes!(values)
+        schedule.as_json
+      end
+
+      desc "Delete a specific schedule event by ID"
+      delete ':_id' do
+        params do
+          requires '_id'
+        end
+        schedule = Illuminati::Models::Schedule.find(params[:_id])
+        error! "Not Found", 404 unless schedule
+        schedule.destroy
+        schedule.as_json
+      end
     end
   end
 end
