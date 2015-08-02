d32cb92fd4af6615a3273c2f4006d95f6899c536
Adam Riddell <riddelladam42@gmail.com>
2015-08-02 22:41:46 +1000

    Include scheduler files in application config

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'api'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'scheduler'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'models'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -7,7 +8,7 @@ require 'boot'
 
 Bundler.require(:default, ENV['RACK_ENV'])
 
-['../../config/initializers/*.rb', '../../models/*.rb', '../../api/*.rb'].each do |path|
+['../../config/initializers/*.rb', '../../models/*.rb', '../../api/*.rb', '../../scheduler/*.rb'].each do |path|
   Dir[File.expand_path(path, __FILE__)].each do |f|
     require f
   end
