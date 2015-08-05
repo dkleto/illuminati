76ff6a82c85c82907c72a58620e3f3a68afb9c30
Adam Riddell <riddelladam42@gmail.com>
2015-08-05 21:21:48 +1000

    Extend scheduler tests for events prior to "now"
        spec/scheduler/scheduler_spec.rb:
            - Make sure that jobs with a time earlier than the current
              time will be ignored by the scheduler.

--- spec/scheduler/scheduler_spec.rb
+++ spec/scheduler/scheduler_spec.rb
@@ -1,39 +1,46 @@
 require 'scheduler_helper'
 
 describe Illuminati::Scheduler do
-  let(:schedule1_hash) {
-      {
-        :command => 'command1',
-        :transition_time => 0,
-        :time => DateTime.now - 1,
-        :repeat => true,
-        :cron_minute => '30',
-        :cron_hour => '17',
-        :cron_day => '*',
-        :cron_month => '*',
-        :cron_weekday => '1,6'
-      }
+  let(:job1_hash) {
+    {
+      :command => 'command1',
+      :transition_time => 2,
+      :time => DateTime.now + 1,
+      :repeat => true,
+      :cron_minute => '30',
+      :cron_hour => '*',
+      :cron_day => '*',
+      :cron_month => '*',
+      :cron_weekday => '*'
+    }
   }
-  let(:schedule2_hash) {
-      {
-        :command => 'command2',
-        :transition_time => 2,
-        :time => DateTime.now + 1,
-        :repeat => true,
-        :cron_minute => '30',
-        :cron_hour => '*',
-        :cron_day => '*',
-        :cron_month => '*',
-        :cron_weekday => '*'
-      }
+  let(:job2_hash) {
+    {
+      :command => 'command2',
+      :transition_time => 0,
+      :time => DateTime.now + 1,
+      :repeat => false
+    }
   }
-  let(:schedule3_hash) {
-      {
-        :command => 'command3',
-        :transition_time => 0,
-        :time => DateTime.now + 1,
-        :repeat => false
-      }
+  let(:job3_hash) {
+    {
+      :command => 'command3',
+      :transition_time => 0,
+      :time => DateTime.now - 1,
+      :repeat => true,
+      :cron_minute => '30',
+      :cron_hour => '17',
+      :cron_day => '*',
+      :cron_month => '*',
+      :cron_weekday => '1,6'
+    }
+  }
+  let(:job4_hash) {
+    {
+      :command => 'command4',
+      :transition_time => 10,
+      :time => DateTime.now - 1
+    }
   }
   context 'with no schedule jobs' do
     it 'does not schedule any changes' do
@@ -45,9 +52,10 @@ describe Illuminati::Scheduler do
   end
   context 'with schedule jobs' do
     before do |each|
-      @job1 = Illuminati::Models::Schedule.create!(schedule1_hash)
-      @job2 = Illuminati::Models::Schedule.create!(schedule2_hash)
-      @job3 = Illuminati::Models::Schedule.create!(schedule3_hash)
+      @job1 = Illuminati::Models::Schedule.create!(job1_hash)
+      @job2 = Illuminati::Models::Schedule.create!(job2_hash)
+      @job3 = Illuminati::Models::Schedule.create!(job3_hash)
+      @job4 = Illuminati::Models::Schedule.create!(job4_hash)
     end
     it 'schedules once-off jobs on initialisation' do
       event_count_before = Rufus::Scheduler.singleton.at_jobs.count
