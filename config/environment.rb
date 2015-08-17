5e05ce596f339d3dd84d59efe3ea9059276b6f5b
Adam Riddell <riddelladam42@gmail.com>
2015-08-18 08:27:39 +1000

    Use tmp dir for logging by default
        config/environment.rb:
            - Use the tmp dir for logging by default, as otherwise
              CI tests will fail.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 ENV['RACK_ENV'] ||= 'test'
-ENV['illuminati.logpath'] ||= '/var/log/sitelogs/illuminati/illuminati.log'
+ENV['illuminati.logpath'] ||= '/tmp/illuminati.log'
 
 require File.expand_path('../application', __FILE__)
