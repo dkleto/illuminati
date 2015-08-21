3da21f2f2ae4b33ef4fb2ed063963141b5b712a3
Adam Riddell <riddelladam42@gmail.com>
2015-08-21 22:36:39 +1000

    Set up logging dir to work with travis CI
        .travis.yml
        config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 ENV['RACK_ENV'] ||= 'test'
-ENV['illuminati.logpath'] ||= '/tmp/illuminati.log'
+ENV['illuminati.logpath'] ||= '/var/log/illuminati/app.log'
 
 require File.expand_path('../application', __FILE__)
