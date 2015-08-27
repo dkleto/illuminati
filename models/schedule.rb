cc5e0dfb9d5994925b3b1409274c97b7783aae48
Adam Riddell <riddelladam42@gmail.com>
2015-08-27 17:29:32 +1000

    Change mongoid model to use hash for xy and huesat
        models/schedule.rb

--- models/schedule.rb
+++ models/schedule.rb
@@ -5,10 +5,9 @@ module Illuminati
 
       field :on, type: Boolean
       field :bri, type: Integer
-      field :hue, type: Integer
-      field :sat, type: Integer
+      field :huesat, type: Hash
       field :alert, type: String
-      field :xy, type: Array
+      field :xy, type: Hash
       field :transitiontime, type: Integer
       field :time, type: DateTime
       field :repeat, type: Boolean
