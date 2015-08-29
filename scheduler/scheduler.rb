8a254bff2f81e28b6eb3ea399da163cdad4b508e
Adam Riddell <riddelladam42@gmail.com>
2015-08-29 23:20:52 +1000

    Update scheduler to handle new cron field format
        scheduler/scheduler.rb:
            - Update scheduler to deal with cron fields as hashes.

--- scheduler/scheduler.rb
+++ scheduler/scheduler.rb
@@ -6,26 +6,25 @@ module Illuminati
     end
 
     def sync
-      repeat_jobs = @schedule.where(:repeat => true)
-      onceoff_jobs = @schedule.any_of({:repeat => false},
-                                      {:repeat => nil},
-                                      {:repeat.exists => false})
+      repeat_jobs = @schedule.where(:cron.ne => nil)
+      onceoff_jobs = @schedule.any_of({:cron => nil},
+                                      {:cron.exists => false})
       onceoff_jobs.each do |job|
         @scheduler.at job[:time].to_s do
           puts job[:command]
         end
       end
       repeat_jobs.each do |job|
-        cron_string = "#{job[:cron_minute]} #{job[:cron_hour]} " +
-                      "#{job[:cron_day]} #{job[:cron_month]} " +
-                      "#{job[:cron_weekday]}"
+        cron_string = "#{job[:cron][:minute]} #{job[:cron][:hour]} " +
+                      "#{job[:cron][:day]} #{job[:cron][:month]} " +
+                      "#{job[:cron][:weekday]}"
         if job[:time] > DateTime.now then
           @scheduler.cron cron_string, :first_at => job[:time] do
-            puts job[:command]
+            puts ''
           end
         else
           @scheduler.cron cron_string do
-            puts job[:command]
+            puts ''
           end
         end
       end
