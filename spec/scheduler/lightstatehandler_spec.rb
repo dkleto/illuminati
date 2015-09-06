47c42635b544d141f0db48d15c3bb6470d44b54c
Adam Riddell <riddelladam42@gmail.com>
2015-09-06 23:24:43 +1000

    Add basic spec for lightstatehandler class
        spec/scheduler/lightstatehandler_spec.rb

---
+++ spec/scheduler/lightstatehandler_spec.rb
@@ -0,0 +1,28 @@
+require 'unit_helper'
+
+require File.expand_path('../../../scheduler/lightstatehandler.rb', __FILE__)
+require File.expand_path('../../../models/schedule.rb', __FILE__)
+
+describe 'lightstatehandler object' do
+  let(:test_state) {
+    {
+      'on' => true, 'bri' => 255, 'hue' => 35000, 'sat' => 255,
+      'alert' => 'none', 'transitiontime' => 5, 'reachable' => true
+    }
+  }
+  let(:schedule) do
+    instance_double('Illuminati::Models::Schedule', :light_state => test_state)
+  end
+
+  it 'does nothing if lights object is nil' do
+    handler = Illuminati::LightStateHandler.new(nil, schedule)
+    expect(handler.call(nil, nil)).to be_nil
+  end
+
+  it 'attempts to set light state' do
+    lights = instance_double('Lights', :set_group_state => true)
+    handler = Illuminati::LightStateHandler.new(lights, schedule)
+    expect(lights).to receive(:set_group_state).with(0, instance_of(BulbState))
+    handler.call(nil, nil)
+  end
+end
