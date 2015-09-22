845108925a6b1265b9db4257c61ff6128dc6e89b
Adam Riddell <riddelladam42@gmail.com>
2015-09-22 23:00:32 +1000

    Add lights config path to environment configuration
        config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 ENV['RACK_ENV'] ||= 'test'
 ENV['illuminati.logpath'] ||= '/tmp/illuminati.log'
+ENV['illuminati.lightsconfigpath'] ||= '/home/illuminati/.lightsconfig'
 
 require File.expand_path('../application', __FILE__)
