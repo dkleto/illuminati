cdd11284637a3d21493241721ff7c6e9612502d8
Adam Riddell <riddelladam42@gmail.com>
2016-01-25 23:14:26 +1100

    Add rough implementation of direct light update

---
+++ api/light.rb
@@ -0,0 +1,60 @@
+module Illuminati
+  class Light < Grape::API
+    format :json
+    rescue_from Grape::Exceptions::ValidationErrors do |e|
+      error! e, 400
+    end
+
+    logger $logger
+
+    helpers do
+      def get_lights
+        lightsconfig = ENV['illuminati.lightsconfigpath']
+        @hue ||= Illuminati.load_lights(lightsconfig, logger)
+      end
+      def logger
+        API.logger
+      end
+    end
+
+    namespace :lights do
+      desc "Updates all lights at once"
+
+      params do
+        optional :xy, type: Hash do
+          requires :x, type: Float, values: 0.0..1.0
+          requires :y, type: Float, values: 0.0..1.0
+        end
+        optional :on, type: Boolean, default: true
+        optional :bri, type: Integer, values: 0..255, default: 255
+        optional :alert, type: String, values: ['none', 'lselect'],
+                 default: 'none'
+        optional :transitiontime, type: Integer, values: 0..1800, default: 0
+        at_least_one_of :xy, :on, :bri, :alert, :transitiontime
+      end
+      put :all do
+        get_lights
+        values = Hash.new
+        declared_params = declared(params, include_missing: false)
+        declared_params.each do |key, value|
+          if !value.nil?
+            if key == 'xy'
+              values['xy'] = [value['x'], value['y']]
+            else
+              values[key] = value
+            end
+          end
+          if @hue.nil? then
+            $logger.info "No lights object available"
+            nil
+          else
+            b = BulbState.new(values)
+            @hue.set_group_state(0,b)
+          end
+        end
+        values.as_json
+      end
+
+    end
+  end
+end
--- app/api.rb
+++ app/api.rb
@@ -3,6 +3,7 @@ module Illuminati
     prefix 'api'
     format :json
     mount ::Illuminati::Schedule
+    mount ::Illuminati::Light
     add_swagger_documentation hide_format: true
   end
 end
